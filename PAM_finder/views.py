04fcdc1101350137eeb95f5ad943e0a978ce06bb
roshan <roshankr2002@gmail.com>
2022-09-23 16:26:36 +0530

    Django Implementation Completed !

--- PAM_finder/views.py
+++ PAM_finder/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from PAM_finder import SearchingAlgorithm
 from django.core.files.storage import FileSystemStorage
+from django.views.decorators import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
     return render(request, 'index.html')
 
 
+@csrf_exempt
 def finder_tool(request):
     return render(request, 'finder_tool.html')
 
 
+@csrf_exempt
 def about_us(request):
     return render(request, 'about_us.html')
 
 
+@csrf_exempt
 def result(request):
     if request.method == 'POST':
         file_text = request.FILES['file']
